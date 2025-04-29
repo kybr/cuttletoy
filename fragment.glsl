4b85d96d9ce286619b0c2a8172439e4c183f28eb
Karl Yerkes <karl.yerkes@gmail.com>
2025-04-28 17:47:11 -0700

    better default content

--- fragment.glsl
+++ fragment.glsl
@@ -1,14 +1,12 @@
-uniform float u_time;
 uniform vec2 u_size;
 
 void main() {
   vec2 p = gl_FragCoord.xy / u_size;
-  vec2 uv = p * 2.0 - 1.0;
-  uv.x *= u_size.x / u_size.y;
-  float d = length(uv);
-  if (d < 0.5) {
-    gl_FragColor = vec4(0);
-  } else {
+  p = p * 2.0 - 1.0;
+  p.x *= u_size.x / u_size.y;
+  if (length(p) < 0.5) {
     gl_FragColor = vec4(1);
+  } else {
+    discard;
   }
 }
